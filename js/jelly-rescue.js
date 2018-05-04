c4bbf7bd178bf68ca0b5cbc32982345846f16d29
santisanchez <santisanchez.1214@gmail.com>
2018-05-04 15:17:10 -0500

    Added jelly rescue page and resource images

---
+++ js/jelly-rescue.js
@@ -0,0 +1,10 @@
+$('.portfolio-item').magnificPopup({
+    type: 'inline',
+    preloader: false,
+    focus: '#username',
+    modal: true
+});
+$(document).on('click', '.portfolio-modal-dismiss', function (e) {
+    e.preventDefault();
+    $.magnificPopup.close();
+});
