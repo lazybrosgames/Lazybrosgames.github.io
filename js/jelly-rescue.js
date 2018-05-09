8c80094b874ae91c6847478fff2616674df4c034
santisanchez <santisanchez.1214@gmail.com>
2018-05-08 21:17:46 -0500

    Added javascript code to send analytics and redirect on callback

--- js/jelly-rescue.js
+++ js/jelly-rescue.js
@@ -8,3 +8,20 @@ $(document).on('click', '.portfolio-modal-dismiss', function (e) {
     e.preventDefault();
     $.magnificPopup.close();
 });
+var html5 = document.getElementById('html5-link');
+
+html5.addEventListener('click', function (event) {
+
+    event.preventDefault();
+
+    ga('send', 'event', 'Platform', 'click', 'Itchio', {
+        hitCallback: function () {
+            window.location.replace('https://lazybrosgames.itch.io/jellyrescue');
+        }
+    });
+    ga('send', 'event', 'Signup Form', 'submit', {
+        hitCallback: function () {
+            form.submit();
+        }
+    });
+});
