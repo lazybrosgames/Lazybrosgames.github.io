44d24c99009be4b4a3ecd74ee23083d3b521542f
santisanchez <santisanchez.1214@gmail.com>
2018-05-08 23:31:47 -0500

    ga modified

--- js/jelly-rescue.js
+++ js/jelly-rescue.js
@@ -8,15 +8,3 @@ $(document).on('click', '.portfolio-modal-dismiss', function (e) {
     e.preventDefault();
     $.magnificPopup.close();
 });
-var html5 = document.getElementById('html5-link');
-
-html5.addEventListener('click', function (event) {
-
-    event.preventDefault();
-
-    ga('send', 'event', 'Platform', 'click', 'Itchio', {
-        hitCallback: function () {
-            window.location.replace('https://lazybrosgames.itch.io/jellyrescue');
-        }
-    });
-});
